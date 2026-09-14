8ed76889fae870d3f133a4169aa8b8f0bcf8ae5b
longidistributor-debug <longidistributor@gmail.com>
2026-09-14 23:11:29 +0500

    Bump MH Analysis to v3 structure lifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
     }
 
     buildTypes {
