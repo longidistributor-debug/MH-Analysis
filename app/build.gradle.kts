6f07c8c280d34b0d9993494ed18147b4a997484d
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 15:24:55 +0500

    Restore Android build and add WebSocket dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,31 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.android")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mh.analysis"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.mh.analysis"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 19
+        versionName = "19.0"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
