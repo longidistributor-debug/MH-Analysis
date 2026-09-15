9be7e1ea4ded5a6fbf8dfd01cc0935bb59a952b3
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 20:14:50 +0500

    Bump MH Analysis to v32

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 31
-        versionName = "31.0"
+        versionCode = 32
+        versionName = "32.0"
     }
 
     buildTypes {
