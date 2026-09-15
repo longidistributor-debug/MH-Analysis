ec13fe6cc155bb0de162d80a7f99b50d94c7f35c
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 16:35:42 +0500

    Bump MH Analysis to v24 WebSocket-first build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 23
-        versionName = "23.0"
+        versionCode = 24
+        versionName = "24.0"
     }
 
     buildTypes {
