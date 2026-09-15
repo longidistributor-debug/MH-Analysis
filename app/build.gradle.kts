78ebd94b484a3d6519bf09ba91d4d731e52fcfcc
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 16:00:22 +0500

    Bump MH Analysis to v21 socket stability build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 20
-        versionName = "20.0"
+        versionCode = 21
+        versionName = "21.0"
     }
 
     buildTypes {
