a5f04852e9b19867988f0d062c6ae43a1a6fa081
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 15:24:43 +0500

    Add OkHttp WebSocket and bump live socket build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,4 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.mh.analysis"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.mh.analysis"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 18
-        versionName = "18.0"
-    }
-
-    buildTypes {
-        release { isMinifyEnabled = false }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
+    id("org.jetbrains.android")
 }
