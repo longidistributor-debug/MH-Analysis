451c5efb185b3969485110a8e9ca5771d1c7a430
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 17:02:20 +0500

    Bump MH Analysis to v25 single socket engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 24
-        versionName = "24.0"
+        versionCode = 25
+        versionName = "25.0"
     }
 
     buildTypes {
