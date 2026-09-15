db652d81ac34e50af6502715c84b6eaec6b3d1b4
longidistributor-debug <longidistributor@gmail.com>
2026-09-15 18:35:56 +0500

    Bump MH Analysis to v28 exact FCS transport

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mh.analysis"
         minSdk = 26
         targetSdk = 33
-        versionCode = 27
-        versionName = "27.0"
+        versionCode = 28
+        versionName = "28.0"
     }
 
     buildTypes {
